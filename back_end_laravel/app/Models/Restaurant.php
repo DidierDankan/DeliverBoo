867209b6f18427e1819907b8b5d2a171003e6e08
NiccoCorsini <nik.piky@gmail.com>
2021-07-09 10:51:28 +0200

    deliveboo db confirmed structure done

---
+++ back_end_laravel/app/Models/Restaurant.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Restaurant extends Model
+{
+    use HasFactory;
+
+    protected $table = 'restaurants';
+
+    protected $fillable = [
+        'name',
+        'address',
+        'city',
+        'zip_code',
+        'cover'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function types(){
+        return $this->belongsToMany(Type::class);
+    }
+
+    public function foods(){
+        return $this->hasMany(Food::class);
+    }
+
+    public function orders(){
+        return $this->hasMany(Order::class);
+    }
+}
