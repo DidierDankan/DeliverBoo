50c87a09a984a5352fb3adb26df71432fe559767
MirkoGriffo <mirkogriffo@yahoo.it>
2021-07-10 17:13:05 +0200

    crud Restaurant

---
+++ back_end_laravel/app/Http/Controllers/Admin/RestaurantController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Restaurant;
+use App\Models\Type;
+
+class RestaurantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $restaurants = Restaurant::all();
+        return view('admin.restaurants.index', compact('restaurants'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+        $types = Type::all();
+        return view('admin.restaurants.create', compact('types'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $new_restaurant = new Restaurant();
+
+        $new_restaurant->fill($data);
+
+        $new_restaurant->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $restaurant = Restaurant::find($id);
+
+        if (!$restaurant) {
+            abort(404);
+        }
+
+        return view('admin.restaurants.show', compact('restaurant'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $restaurant = Restaurant::find($id);
+        $types = Type::all();
+
+        if (!$restaurant) {
+            abort(404);
+        }
+        return view('admin.restaurants.edit', compact('restaurant', 'types'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $restaurant = Restaurant::find($id);
+
+        $restaurant->delete();
+
+        return redirect()->route('admin.restaurants.index')->with('deleted', $restaurant->name);
+    }
+}
--- back_end_laravel/routes/web.php
+++ back_end_laravel/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->middleware('auth')
+    ->name('admin.')
+    ->group(function () {
+       
+        Route::get('/', 'HomeController@index')->name('home');
+
+        
+        Route::resource('/restaurants', 'RestaurantController');
+    });
