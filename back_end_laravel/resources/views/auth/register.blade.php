7c90af2e3e58f428e50e7da5bbfee0c40de252e9
salvatorevarisano <salvator3varisano@gmail.com>
2021-07-10 10:45:32 +0200

    admin-routes

--- back_end_laravel/resources/views/auth/register.blade.php
+++ back_end_laravel/resources/views/auth/register.blade.php
@@ -65,7 +65,7 @@
                             <label for="cf" class="col-md-4 col-form-label text-md-right">{{ __('C.F.') }}</label>
 
                             <div class="col-md-6">
-                                <input id="cf" type="text" class="form-control @error('cf') is-invalid @enderror" name="cf" required autocomplete="new-cf">
+                                <input id="cf" type="text" class="form-control @error('cf') is-invalid @enderror" name="cf" autocomplete="new-cf">
 
                                 @error('cf')
                                     <span class="invalid-feedback" role="alert">
