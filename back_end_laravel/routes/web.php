1ded0c1d9028efe9becc9ff62dfbdbffb754869b
MirkoGriffo <mirkogriffo@yahoo.it>
2021-07-10 17:21:38 +0200

    restaurant crud

--- back_end_laravel/routes/web.php
+++ back_end_laravel/routes/web.php
@@ -23,7 +23,7 @@ Route::prefix('admin')
     ->name('admin.')
     ->group(function () {
        
-        Route::get('/', 'HomeController@index')->name('home');
+        Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
         
         Route::resource('/restaurants', 'RestaurantController');
