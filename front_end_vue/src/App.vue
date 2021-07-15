61e2f45b59140dddac4051636a53994281227579
salvatorevarisano <salvator3varisano@gmail.com>
2021-07-15 12:14:55 +0200

    vue hero componet

--- front_end_vue/src/App.vue
+++ front_end_vue/src/App.vue
@@ -8,24 +8,8 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import '@/style/vars.scss';
+@import '@/style/general.scss';
 
-#nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
