c5554012e9dbd91aa6d828c90da5fb2a8b714538
DidierDankan <didi_archer@hotmail.com>
2021-07-16 15:43:08 +0100

    fix header

--- front_end_vue/src/views/components/Header.vue
+++ front_end_vue/src/views/components/Header.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="container-header">
-    <router-link class="links brand" to="/">DeliveBoo</router-link>
-    <div id="nav">
-      <router-link class="links" to="/">Home</router-link> |
-      <router-link class="links" to="/about">About</router-link> |
-      <router-link class="links" to="/restaurants">Restaurants</router-link>
+  <div class="bg-container">
+    <div class="container-header">
+      <router-link class="links brand" to="/">DeliveBoo</router-link>
+      <div id="nav">
+        <router-link class="links" to="/">Home</router-link> |
+        <router-link class="links" to="/about">About</router-link> |
+        <router-link class="links" to="/restaurants">Restaurants</router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -15,22 +17,30 @@ export default {
 };
 </script>
 
-<style lang="scss">
-.container-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px;
+<style scoped lang="scss">
+.bg-container {
   background-color: #00ccbc;
-  .links {
-    text-decoration: none;
-    color: #fff;
-  }
-  .brand {
-    font-size: 2rem;
-  }
-  #nav {
-    padding: 10px;
+  .container-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 1170px;
+    margin: 0 auto 20px;
+    padding: 10px 0px;
+    .links {
+      text-decoration: none;
+      color: #fff;
+      &:hover {
+        color: rgb(228, 222, 222);
+      }
+    }
+    .brand {
+      margin-left: 1rem;
+      font-size: 2rem;
+    }
+    #nav {
+      margin-right: 1rem;
+    }
   }
 }
 </style>
