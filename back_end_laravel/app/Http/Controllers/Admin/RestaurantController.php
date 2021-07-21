72ddfbb0d5970cd71d4271cdd43ecf9f8858c195
PriscoCoppola <priscocoppola7@gmail.com>
2021-07-21 16:36:16 +0200

    clear code back end

--- back_end_laravel/app/Http/Controllers/Admin/RestaurantController.php
+++ back_end_laravel/app/Http/Controllers/Admin/RestaurantController.php
@@ -7,11 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\Restaurant;
 use App\Models\Food;
 use App\Models\User;
-
 use Illuminate\Support\Facades\Storage;
-
 use Illuminate\Support\Facades\Auth;
-
 use App\Models\Type;
 use Illuminate\Validation\Rule;
 
@@ -24,11 +21,9 @@ class RestaurantController extends Controller
      */
     public function index()
     {
-        
         $user_id = Auth::user()->id;
 
         $types = Type::all();
-
 
         $restaurants = Restaurant::where('user_id', '=', $user_id)->paginate(6);
 
@@ -42,10 +37,8 @@ class RestaurantController extends Controller
      */
     public function create()
     {
-
         $types = Type::all();
         return view('admin.restaurants.create', compact('types'));
-        // return 'peppino';
     }
 
     /**
@@ -57,9 +50,6 @@ class RestaurantController extends Controller
     public function store(Request $request)
 
     {   
-
-        // dd($request->all());
-
         $request->validate([
             'name' => ['required', 'max:255', 'unique:restaurants'],
             'address' => ['required','max:255'],
@@ -75,17 +65,11 @@ class RestaurantController extends Controller
             'mimes'=> ':attribute is of unsupported format'
         ]);
 
-
         if ($request->hasFile('cover')) { // depends on your FormRequest validation if `file` field is required or not
             $path = $request->cover->storePublicly('');
         }
-    
-        // Restaurant::create(array_merge($request->except('cover'), ['cover' => $path]));
-
 
         $data = $request->all();
-
-        dd($data);
 
         $new_restaurant = new Restaurant();
 
@@ -93,15 +77,10 @@ class RestaurantController extends Controller
 
         $new_restaurant->user_id = Auth::user()->id;
 
-
-
         if ($request->hasFile('cover')){
 
             $new_restaurant->cover = $path;
         }
-
-
-
 
         $new_restaurant->save();
 
@@ -116,7 +95,6 @@ class RestaurantController extends Controller
         }
 
         return redirect()->route('admin.restaurants.show', $new_restaurant->id)->with('created', $new_restaurant->name);
-
     }
 
     /**
@@ -132,14 +110,11 @@ class RestaurantController extends Controller
 
         $restaurant = Restaurant::where('user_id', '=', $user_id)->find($id);
 
-
         $foods = Food::where('restaurant_id', '=', $id)->paginate(4);
 
         $types = Type::all();
 
         $user = User::find($user_id);
-
-        // dd($user);
 
         if (!$restaurant) {
             return view('admin.errors.404error');
@@ -156,15 +131,10 @@ class RestaurantController extends Controller
      */
     public function edit($id)
     {
-
         $user_id = Auth::user()->id;
 
         $restaurant = Restaurant::where('user_id', '=', $user_id)->find($id);
 
-
-        
-
-        
         $types = Type::all();
 
         if (!$restaurant) {
@@ -182,14 +152,8 @@ class RestaurantController extends Controller
      */
     public function update(Request $request, $id)
     {
-
         $request->validate([
-            'name' => [
-                'required', 
-                Rule::unique('restaurants')->ignore($id),
-                'max:255',
-                
-            ],
+            'name' => ['required', Rule::unique('restaurants')->ignore($id), 'max:255'],
             'address' => ['required','max:255'],
             'city' => ['required','max:50'],
             'zip_code' => ['required', 'max:10', 'string', 'size:5'],
@@ -203,22 +167,14 @@ class RestaurantController extends Controller
             'mimes'=> ':attribute is of unsupported format'
         ]);
 
-
-
         if ($request->hasFile('cover')) { 
             $path = $request->cover->storePublicly('');
 
         }
-
-        
-
-        
         
         $data = $request->all();
         
         $restaurant = Restaurant::find($id);
-        
-        
         
         if (array_key_exists('types', $data)) {
             //aggiungere record tabella pivot
@@ -227,8 +183,7 @@ class RestaurantController extends Controller
         } else {
             $restaurant->types()->detach(); //rimuove tutte le records nella pivot
         }
-        
-            
+          
             if(array_key_exists('cover', $data)){
                 
                 if($restaurant->cover){
@@ -237,15 +192,12 @@ class RestaurantController extends Controller
 
                 $restaurant->cover = $path;
 
-                $data['cover'] = Storage::put('public/restaurants-covers/', $data['cover']);
-                
+                $data['cover'] = Storage::put('public/restaurants-covers/', $data['cover']); 
             }
             
             $restaurant->update($data);
 
-
         return redirect()->route('admin.restaurants.show', $restaurant->id);
-
     }
 
     /**
@@ -261,6 +213,5 @@ class RestaurantController extends Controller
         $restaurant->delete();
 
         return redirect()->route('admin.restaurants.index')->with('deleted', $restaurant->name);
-
     }
 }
